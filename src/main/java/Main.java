13118f97136a73b863e34353bee5225762a4af5b
Tray Denney <denney.tray@gmail.com>
2019-01-07 05:28:51 -0600

    AWS support
    AWS support added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,24 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.permission.*;
 
-public class Main {
-    public static void main( String[] args ) {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
+
+// run gradle build
+
+public class Main {
+    public static void main( String token, Context context ) {
+
+        LambdaLogger logger = context.getLogger();
+
+//        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
         DiscordApi api = new DiscordApiBuilder().setToken(System.getenv("token")).login().join();
 
         Permissions permissions = new PermissionsBuilder().setState(PermissionType.MANAGE_ROLES, PermissionState.ALLOWED).build();
+
+//        logger.log("Bot has successfully started");
+//        logger.log("You can invite the bot by using the following url: " + api.createBotInvite(permissions));
 
         System.out.println("Bot has successfully started");
         System.out.println("You can invite the bot by using the following url: " + api.createBotInvite(permissions));
@@ -29,5 +41,6 @@ public class Main {
                 e.printStackTrace();
             }
         });
+
     }
 }
